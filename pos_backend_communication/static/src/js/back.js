145a21e1567597bbfb22198b38e33205c731cec8
Reinier Reisy Quevedo Batista <reisyreinier@gmail.com>
2019-06-03 17:58:26 -0400

    [FIX] pos_backend_communication: Enviando post message a todas las pestañas, en teoría sólo debe haber una abierta.
    Change-Id: Id385714b916c530424940f28e01bc1b0f092ed2b

--- pos_backend_communication/static/src/js/back.js
+++ pos_backend_communication/static/src/js/back.js
@@ -14,7 +14,7 @@ odoo.define('pos_backend_communication.back', function (require) {
         //send message to pos
         if (is_tied_to_pos()) {
             //can only work if the backoffice is opened by the POS
-            window.opener.postMessage(a, location.origin);
+            window.opener.postMessage(a, '*');
         }
     }
 
